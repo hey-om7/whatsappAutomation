fc139f1b98545a65ea85780c441a8feacc5524b2
Om Ambarkar <heyom7@gmail.com>
2022-01-17 23:00:30 +0530

    stable release

--- functionAutomation.py
+++ functionAutomation.py
@@ -2,13 +2,13 @@ import pyautogui as pya
 from time import sleep
 from extractNumbers import extractor
 from playMusic import soundPlay
-from readingWriting import saveNos,read
-
+from readingWriting import saveNos,read, write
+import os
 
 url=r'''https://web.whatsapp.com/send?phone='''
 def startAutomation(userList,sendMsgList):
-    sleep(3)
-    for user in userList:
+    sleep(10)
+    for index,user in enumerate(userList):
         pya.hotkey('ctrl','t')
         sleep(3)
         pya.write(url+user)
@@ -40,7 +40,9 @@ def startAutomation(userList,sendMsgList):
             sleep(1)
         sleep(12)
         pya.hotkey('ctrl','w')
-        sleep(2)
+        sleep(1)
+        write("midProcessing.txt",str(userList[index:]))
+        sleep(1)
 
 
 
@@ -49,8 +51,13 @@ def initAuto(inputImpureNosString:str,userMsgToSend:str):
     userList=list(map(lambda x:x.replace("+",""),userList))
     userList=set(userList)-set(read("previousNumbers.txt"))
     userList=list(userList)
-    print(f"assuming 1 min for each you require{len(userList)} mins")
-    startAutomation(userList=userList,sendMsgList=userMsgToSend)
+    if not (os.path.isfile("midProcessing.txt")):
+        write("midProcessing.txt",str(userList))
+        
+    print(f"Assuming 1 min for each you number you require:",len(read("midProcessing.txt")),"mins")
+    
+    startAutomation(userList=read("midProcessing.txt"),sendMsgList=userMsgToSend)
+    os.remove("midProcessing.txt")
     saveNos(userList)
     while True:
         soundPlay('sfx1.mp3')
--- playMusic.py
+++ playMusic.py
@@ -1,5 +1,3 @@
-#https://web.whatsapp.com/send?phone=0000000000000
-
 import playsound
 
 def soundPlay(name:str):
